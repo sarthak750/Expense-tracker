efb06b5abbd31f9df2b2b2087e597405ab68621c
prakhar <gupta.19@iitj.ac.in>
2024-01-12 13:36:00 +0530

    U

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ExpenseForm from "./ExpenseForm.js";
+import ExpenseSummary from "./ExpenseSummary.js";
+import ExpensesList from "./ExpenseList.js";
+import axios from "axios";
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpense = (expense) => {
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+
+  const deleteExpense = (id) => {
+    setExpenses((prevExpenses) => prevExpenses.filter((exp) => exp.id !== id));
+  };
+
+  useEffect(() => {
+    //axios call
+    axios
+      .get("http://localhost:8200")
+      .then((res) => {
+        console.log("response", res.data.message);
+        if (res.data.message == "success") {
+          //success
+        } else {
+          // error no data
+        }
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
+      <ExpenseForm onAddExpense={addExpense} />
+      <ExpensesList expenses={expenses} onDeleteExpense={deleteExpense} />
+      <ExpenseSummary expenses={expenses} />
     </div>
   );
 }
